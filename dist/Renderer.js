c7f9255a69711aef55922d77a2c63f50269cf7c7
Leah Chercham <leahchercham@hotmail.com>
2019-09-11 13:09:20 -0500

    added Handlebars template

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -2,9 +2,16 @@ const Renderer = function(){
 
     const renderData = function(allCityData){
 
+        console.log(allCityData) // to see if it is already an object
+
+        const source = $("weather-template").html()
+        const template = Handlebars.compile(source)
+        const newHtml = template(allCityData)
+        $("#main").append(newHtml)
+
     }
 
     return{
-
+        renderData: renderData
     }
 }
