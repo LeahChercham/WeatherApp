919cda77dbfe1feb9fced85af6d31a0a4b73c3d4
Leah Chercham <leahchercham@hotmail.com>
2019-09-12 13:09:27 -0500

    test

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -3,9 +3,10 @@ class Renderer {
     renderData(allCityData){
         $("#main").empty()
 
+        if(allCityData != []){
         const source = $("#weather-template").html()
         const template = Handlebars.compile(source)
         const newHtml = template({allCityData})
-        $("#main").append(newHtml)
+        $("#main").append(newHtml)} else{console.log("No data in DB")}
     }
 }
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WeatherDB", {us
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use(express.static(path.join(__dirname, "/dist")))
-app.use(express.static(path.join(__dirname, "/node_modules")))
+app.use(express.static(path.join(__dirname, "dist")))
+app.use(express.static(path.join(__dirname, "node_modules")))
 app.use("/", api)
 
 
